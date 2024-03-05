f3e0cee2e11edac95a1acad22c596a076d73eebc
Lucio Benini <iiriya@iiriya.com>
2024-03-05 23:49:07 +0100

    Feat: table keys

--- src/index.js
+++ src/index.js
@@ -35,15 +35,62 @@ exports.dataset = async (client, name) => {
  *
  * @param {object} dataset BigQuery dataset.
  * @param {string} name ID or name.
- * @param {object} options
+ * @param {object} options Creation options.
+ * @param {object} keys Keys options. Object can contains primary and foreign.
  * @returns The table with the given name.
  */
-exports.table = async (dataset, name, options) => {
+exports.table = async (dataset, name, options, keys) => {
   const table = dataset.table(name)
   const [status] = await table.exists()
 
   if (!status) {
-    return await table.create(options)
+    const result = await table.create(options)
+
+    if (keys) {
+      if (typeof keys.primary !== "undefined") {
+        let pk = ""
+        if (Array.isArray(keys.primary)) {
+          pk = keys.primary.join(", ")
+        } else {
+          pk = keys.primary
+        }
+
+        const [job] = await dataset.bigQuery.createQueryJob({
+          query: `ALTER TABLE \`${dataset.id}.${name}\` ADD PRIMARY KEY (${pk}) NOT ENFORCED;`,
+        })
+
+        if (typeof keys.foreign !== "undefined") {
+          let fks = []
+
+          if (!Array.isArray(keys.foreign)) {
+            fks = [keys.foreign]
+          } else {
+            fks = keys.foreign
+          }
+
+          await job.getQueryResults()
+
+          for (index in fks) {
+            let fk = ""
+            if (Array.isArray(fks[index].key)) {
+              fk = fks[index].key.join(", ")
+            } else {
+              fk = fks[index].key
+            }
+
+            const [fkJob] = await dataset.bigQuery.createQueryJob({
+              query: `ALTER TABLE \`${dataset.id}.${name}\` ADD CONSTRAINT ${fks[index].name} FOREIGN KEY (${fk}) REFERENCES ${dataset.id}.${fks[index].table}(${fks[index].reference || pk}) NOT ENFORCED;`,
+            })
+
+            await fkJob.getQueryResults()
+          }
+        } else {
+          await job.getQueryResults()
+        }
+      }
+    }
+
+    return result
   } else {
     return await table.get()
   }
